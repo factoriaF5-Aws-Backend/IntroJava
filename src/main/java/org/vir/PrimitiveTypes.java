36cb7461a598009d0a04872d526238d84ab8d411
Sergi Virgili <svirgilif@gmail.com>
2024-10-28 10:29:00 +0100

    feat: boolean type

---
+++ src/main/java/org/vir/PrimitiveTypes.java
@@ -0,0 +1,13 @@
+package org.vir;
+
+public class PrimitiveTypes {
+
+    public static void main(String[] args) {
+
+        // escribir un boolean, asignarlo, mostrar por consola
+        boolean isActive = true;
+
+        System.out.println(isActive);
+    }
+
+}
