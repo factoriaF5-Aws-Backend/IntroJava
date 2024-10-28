58c1f22eff27a119cab701311996b45d90e00540
Sergi Virgili <svirgilif@gmail.com>
2024-10-28 18:46:24 +0100

    basics: conditionals

---
+++ src/main/java/org/vir/basics/Conditionals.java
@@ -0,0 +1,14 @@
+package org.vir.basics;
+
+public class Conditionals {
+
+    public static void main(String[] args) {
+        byte userAge = 17;
+
+        if (userAge >= 18) {
+            System.out.println("El usuario es mayor de edad");
+        } else {
+            System.out.println("El usuario NO es mayor de edad");
+        }
+    }
+}
